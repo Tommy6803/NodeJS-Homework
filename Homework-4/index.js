5a1c3094cd0ca5bb262dc4ec17e4efc51060a936
Artem Abramov <tematmb@yandex.ru>
2022-05-25 22:43:41 +0300

    Homework-4

---
+++ Homework-4/index.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+
+
+
+
+const options = async () => {
+    const serchParams = { dirToSearch: '', pattern: '' };
+
+    const { thisDir } = await inquirer.prompt([{
+        name: 'thisDir',
+        type: 'confirm',
+        message: 'search here:',
+        describe: 'search here'
+    }]);
+
+    if (!thisDir) {
+        serchParams.dirToSearch = (await inquirer.prompt([{
+            name: 'dirToSearch',
+            type: 'input',
+            message: 'indicate the path: ',
+            describe: 'indicate the path'
+        }])).dirToSearch;
+    } else {
+        serchParams.dirToSearch = process.cwd();
+    }
+
+    serchParams.pattern = (await inquirer.prompt([{
+        name: 'pattern',
+        type: 'input',
+        message: 'Pattern: ',
+        describe: 'Pattern'
+    }])).pattern;
+
+    return serchParams;
+};
+
+
+const dir = (dirPath) => fs.lstatSync(dirPath).isDirectory();
+
+
+const run = async () => {
+    const { dirToSearch, pattern } = await options();
+    const files = [];
+    const dirsToResearch = [];
+    dirsToResearch.push(dirToSearch);
+
+    while (dirsToResearch.length > 0) {
+        const currentDir = dirsToResearch.shift();
+        const dirContains = fs.readdirSync(currentDir);
+        const inDirFiles = dirContains.filter((fileName) => fileName.indexOf(pattern) !== -1);
+        const inDirDirs = dirContains.map((dirName) => path.join(currentDir, dirName)).filter(dir);
+        files.push(...inDirFiles);
+        dirsToResearch.push(...inDirDirs);
+    }
+
+    console.log(files);
+};
+
+run();
